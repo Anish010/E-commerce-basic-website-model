1236f6ec193ccd337be51626e367a1d8883bf931
Anish010 <anishmohanty101@gmail.com>
2022-10-16 23:53:54 +0530

    added onChange events to all SignUp Page inputBox class and set the useStates. Added the onClick event to SignUp button

--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -4,17 +4,34 @@ const SignUp = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const collectData = () => {
+    console.warn(name, email, password);
+  };
   return (
     <div className="register">
       <h1>Register</h1>
-      <input className="inputBox" type="text" placeholder="Enter Name" />
-      <input className="inputBox" type="email" placeholder="Enter Email" />
+      <input
+        className="inputBox"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter Name"
+      />
+      <input
+        className="inputBox"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter Email"
+      />
       <input
         className="inputBox"
         type="password"
         placeholder="Enter Passoword"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button className="appButton" type="button">
+      <button className="appButton" type="button" onClick={collectData}>
         Sign Up
       </button>
     </div>
