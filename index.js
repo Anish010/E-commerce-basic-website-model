4f750ac81bceaa8b7b202bbd4a4da57dcd4bc163
Anish010 <anishmohanty101@gmail.com>
2022-10-22 23:58:22 +0530

    connect MongoDB to Nodejs, define models and schema in User.js file, added express.json() middleware, connect MongoDB with react, implemented cors() for cors error prevention

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 //importing module
-const express = require('express');
+const express = require("express");
+require("./db/config");
+//for preventing cors errors
+const cors = require("cors");
+//importing user file
+const User = require("./db/User");
 
 //make express in executable form
 const app = express();
 
-//Send response to the web page
-app.get("/",(req, resp)=>
-{
-    resp.send("App is working!")
+//The express.json() function is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json());
+app.use(cors());
+
+app.post("/signup", async (req, resp) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  resp.send(result);
 });
-app.listen(5000)
+
+app.listen(5000);
