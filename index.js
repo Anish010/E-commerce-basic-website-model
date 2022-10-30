fe0885ee5a22628a93fbd68024d671309c4f2d57
Anish010 <anishmohanty101@gmail.com>
2022-10-30 17:09:59 +0530

    created a route in index.js for api for login. Removed password from register and login api.

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ app.use(cors());
 app.post("/signup", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
+  //convert result to object
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
 });
 
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "no user found" });
+    }
+  } else {
+    resp.send({ result: "password or email is missing" });
+  }
+  //since we don't want password we are removing it. )thats why we used select("-passoword"). Minus here remove the password.
+});
 app.listen(5000);
